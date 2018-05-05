77005232052c228b01edc14040460c8a44a30939
Purple Myst <PurpleMyst@users.noreply.github.com>
2018-05-05 18:06:02 +0200

    Handle visited and relative URLs
    I have some issues with URLs such as "#" or those missing schemas, but I'm pretty weirded out about the fact that hyper does not provide a way to do this.
    
    Also, I clone the Uri struct 3 times, which I'm sure is fine from a memory standpoint, but for some reason in Rust I hate allocation.

--- src/crawler.rs
+++ src/crawler.rs
@@ -1,15 +1,17 @@
 use futures::{Future, Stream};
 use hyper::{client::HttpConnector, Client, Error as HyperError, Uri};
 use hyper_tls::HttpsConnector;
-use select::{document::Document, predicate::Name};
+use select::{document::Document, node::Node};
 use tokio_core::reactor::Handle;
+
+use std::collections::HashSet;
 
 const VISIT_LIMIT: usize = 10;
 
 #[derive(Debug)]
 pub struct Crawler {
     handle: Handle,
-    visited: usize,
+    visited: HashSet<Uri>,
     http_client: Client<HttpsConnector<HttpConnector>>,
 }
 
@@ -17,7 +19,7 @@ impl Crawler {
     pub fn new(handle: &Handle) -> Self {
         Self {
             handle: handle.clone(),
-            visited: 0,
+            visited: HashSet::new(),
             http_client: Client::configure()
                 // TODO Not use unwrap + not hardcode 4
                 .connector(HttpsConnector::new(4, handle).unwrap())
@@ -25,18 +27,22 @@ impl Crawler {
         }
     }
 
-    pub fn visit(&mut self, uri: Uri) -> Option<impl Future<Item = Vec<Uri>, Error = HyperError>> {
-        if self.visited >= VISIT_LIMIT {
+    pub fn visit(
+        &mut self,
+        base_uri: Uri,
+    ) -> Option<impl Future<Item = Vec<Uri>, Error = HyperError>> {
+        if self.visited.len() >= VISIT_LIMIT || self.visited.contains(&base_uri) {
             return None;
         } else {
-            self.visited += 1;
+            self.visited.insert(base_uri.clone());
         }
 
         Some(
             self.http_client
-                .get(uri)
+                // XXX: Can we remove this clone?
+                .get(base_uri.clone())
                 .and_then(|response| response.body().collect())
-                .map(|chunks| {
+                .map(move |chunks| {
                     // TODO: Use something smarter than reading the whole body into memory.
                     let body = chunks
                         .into_iter()
@@ -46,12 +52,34 @@ impl Crawler {
                     let document = Document::from(body.as_str());
 
                     let links = document
-                        .find(Name("a"))
+                        .find(|node: &Node| node.html().starts_with("<a"))
                         .filter_map(|node| node.attr("href"));
 
-                    // TODO: Handle relative links.
                     links
                         .filter_map(|link| link.parse::<Uri>().ok())
+                        .filter_map(|uri| {
+                            if uri.is_absolute() {
+                                Some(uri)
+                            } else {
+                                let new_url = 
+                                    if uri.path().starts_with("//") {
+                                        format!("{}:{}", base_uri.scheme()?, uri)
+                                    } else if uri.to_string() == "#" {
+                                        // TODO: implement a better check for this.
+                                        // We could just return `base_uri.to_string()`, but we've obviously already visited that so we don't care.
+                                        return None;
+                                    } else {
+                                        format!(
+                                            "{}://{}{}",
+                                            uri.scheme().or(base_uri.scheme())?,
+                                            uri.authority().or(base_uri.host())?,
+                                            uri.path(),
+                                        )
+                                    };
+
+                                new_url.parse::<Uri>().ok()
+                            }
+                        })
                         .collect::<Vec<_>>()
                 }),
         )
